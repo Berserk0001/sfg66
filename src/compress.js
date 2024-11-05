fdc4943163750848415c7d637696b8083e9d0f34
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-05 15:27:33 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -10,7 +10,7 @@ const redirect = require('./redirect')
 const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
+  const format = 'jpeg'
 
   /*
    * Determine the uncompressed image size when there's no content-length header.
